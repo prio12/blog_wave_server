7e4d8b34d3aff7b8ab1020b27681acb6b4419473
MRPrio <maksudurprio.mp@gmail.com>
2023-10-23 01:31:04 +0600

    delete and claps update method created

--- index.js
+++ index.js
@@ -34,20 +34,19 @@ async function run() {
       const existingUser = await users.findOne({ uid: uid });
       if (existingUser) {
         return res.status(409).send("User already exist in database!");
-      } 
-      else {
+      } else {
         const result = await users.insertOne(user);
-        console.log(result);
+        // console.log(result);
         res.send(result);
       }
     });
 
     //edit a user's profile
 
-    app.put('/users/:userId', async(req,res) =>{
+    app.put("/users/:userId", async (req, res) => {
       const userId = req.params.userId;
       const content = req.body;
-      const filter = {uid:userId};
+      const filter = { uid: userId };
       const options = { upsert: true };
       const updateDoc = { $set: {} };
       if (content.photoURL) {
@@ -58,55 +57,114 @@ async function run() {
         updateDoc.$set.name = content.displayName;
       }
       if (content.about) {
-        updateDoc.$set.about = content.about 
+        updateDoc.$set.about = content.about;
       }
-      
-      const result = await users.updateOne(filter,updateDoc,options);
-      res.send(result)
-    })
+
+      const result = await users.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
 
     //get a user's Details
-    app.get('/users/:userUid', async (req,res) =>{
+    app.get("/users/:userUid", async (req, res) => {
       const userUid = req.params.userUid;
-      const query = {uid:userUid};
+      const query = { uid: userUid };
       const result = await users.findOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
     //post all blogs
 
-    app.post('/blogs', async (req,res) =>{
+    app.post("/blogs", async (req, res) => {
       const post = req.body;
       const result = await blogs.insertOne(post);
-      res.send(result)
-
-    })
+      res.send(result);
+    });
 
     //get all blogs
 
-    app.get("/blogs", async(req,res) =>{
+    app.get("/blogs", async (req, res) => {
       const query = {};
       const posts = await blogs.find(query).toArray();
-      res.send(posts) 
-    })
+      res.send(posts);
+    });
 
     //get a blog for details
 
-    app.get('/blogs/:blogId', async (req,res) =>{
+    app.get("/blogs/:blogId", async (req, res) => {
       const _id = req.params.blogId;
-      const query = {_id : new ObjectId(_id)};
+      const query = { _id: new ObjectId(_id) };
       const blog = await blogs.findOne(query);
       res.send(blog);
+    });
+
+    //get only a user's blog
+    app.get("/blogs/myBlogs/:uid", async (req, res) => {
+      const uid = req.params.uid;
+      // console.log(uid);
+      const query = { userUid: uid };
+      const result = await blogs.find(query).toArray();
+      res.send(result);
+    });
+
+    //update a user's blog
+    app.put("/blogs/myBlogs/edit/:_id", async (req, res) => {
+      const _id = req.params._id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(_id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          title: data.title,
+          description: data.description,
+          image: data.image,
+        },
+      };
+      const result = blogs.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+    //clap operation
+
+    //  app.put('/blogs/blogDetails/likes/:_id', async (req,res) =>{
+    //   const _id = req.params._id;
+    //   const filter = {_id: new ObjectId(_id)};
+    //   const options = {upsert:true};
+    //   const blog = await blogs.findOne(filter);
+    //   const newClaps = blog.claps + 1;
+    //   const updateDoc = {
+    //     $set:{
+    //       claps:newClaps,
+    //     }
+    //   }
+    //   const result = await blogs.updateOne(filter,updateDoc,options);
+    //   res.send(result);
+    //  })
+    app.put("/blogs/blogDetails/likes/:_id", async (req, res) => {
+      try {
+        const _id = req.params._id;
+        const filter = { _id: new ObjectId(_id) };
+        const options = { upsert: true };
+        const blog = await blogs.findOne(filter);
+        const newClaps = blog.claps + 1;
+        const updateDoc = {
+          $set: {
+            claps: newClaps,
+          },
+        };
+        await blogs.updateOne(filter, updateDoc, options);
+        const updatedBlog = await blogs.findOne(filter); // Retrieve the updated blog after the update
+        res.json(updatedBlog); // Send the updated blog data as the response
+      } catch (error) {
+        res.status(500).json({ error: "Server error" });
+      }
+    });
+
+    //delete a blog
+
+    app.delete("/delete/:_id", async (req,res) =>{
+      const _id = req.params._id;
+      const query = {_id : new ObjectId(_id)};
+      const result = await blogs.deleteOne(query);
+      res.send(result)
     })
-
-   //get only a user's blog
-   app.get('/blogs/myBlogs/:uid',async(req,res) =>{
-    const uid = req.params.uid;
-    console.log(uid);
-    const query = {userUid:uid}
-    const result = await blogs.find(query).toArray();
-    res.send(result)
-   })
-
   } catch (error) {}
 }
 run().catch(console.log);
